785cef66573532ecc7a21f8a1fb845f9e5f81517
Abhishek Jhariya <abhi636kumar@gmail.com>
2024-03-28 16:44:47 +0530

    utils code and helper functions added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import dotenv from "dotenv";
-
-import express from "express";
+import { app } from "./app.js";
 import { connectDB } from "./db/index.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+// --------------------------------
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed !!", err);
+  });
 
 /*
 
